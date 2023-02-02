e7f758bc83ce595440332295c3eb147e7358b40c
Gabbie99 <khoinom2018@gmail.com>
2023-02-02 21:00:41 +0100

    chore: working on service page

--- src/pages/servicePage/ServicePage.jsx
+++ src/pages/servicePage/ServicePage.jsx
@@ -6,10 +6,21 @@ function ServicePage() {
     <div className='grand-Servicecontainer'>
       <div className="grand-Servicebody">
         <div className="grand-Servicetext">
-            <h1 className='grand-Servicehead'>We Offer Wide Range Of Services</h1>
-            <p>
+            <h1 className='grand-Servicehead'>We Offer A Wide Range Of Services</h1>
+            <p className='grand-Servicepara'>
             From conception to completion, our entire team management and experienced executives at <span className='grand-Servicespan'>GRANDSCAPE RESIDENCE'S LIMITED</span>  make our clients' needs and satisfaction a priority. We have experience in a wide variety of projects and delivery methods, and use both time-proven practices and cutting-edge techniques to make sure our customers projects meet their maximum potential.
             </p>
+            <div className="grand-Servicecardswrap">
+                <div className="grand-Servicecards">
+                    <img src="" alt="" />
+                </div>
+                <div className="grand-Servicecards">
+                    <img src="" alt="" />
+                </div>
+                <div className="grand-Servicecards">
+                    <img src="" alt="" />
+                </div>
+            </div>
             <p className='grand-Servicelearn'>
                 <a href="#">Learn More</a>
             </p>
