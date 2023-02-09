c2a2a09ca0fff6e65040f6150e548a85d1be5b5b
Gabbie99 <khoinom2018@gmail.com>
2023-02-10 00:16:48 +0100

    chore: working on contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,31 +10,32 @@ function Contact() {
         <NavBar/>
         <div className="gs-Contactwrap-head">
            <h1 className='gs-Contacthead'>How Can We Help?</h1> 
-           <p>
-           Please feel free to call us to discuss your upcoming project! For inquiries about employment, subcontracting for us, or information about our current project, please use the form below so we can connect you to the best person to help you.
-           </p>
+           
         </div>  
       </div>
       <div className="gs-Contactbody">
         <div className="gs-Contact-text">
             <h1 className='gs-Contact-texthead'>Contact Us</h1>
+                <p className='gs-Contact-text-p'>
+                Please feel free to call us to discuss your upcoming project! For inquiries about employment, subcontracting for us, or information about our current project, please use the form below so we can connect you to the best person to help you.
+                </p>
+        </div>
             <div className="gs-Contactflex">
+                <div className='gs-Contactmap'>
+                    <iframe src="" frameborder="0"></iframe>
+                </div>
                 <div className='gs-Contactform'>
                     <form action="submit">
-                        <input type="text" className='gs-input' placeholder='Full Name*' required /><br />
-                        <input type="text" className='gs-input' placeholder='Email Address*' required /><br />
-                        <input type="text" className='gs-input' placeholder='Phone No*' required /><br />
-                        <textarea name="text" id="text" cols="30" rows="10" placeholder='Message...' required></textarea>
+                        <p>Name*</p><input type="text" className='gs-input' placeholder='Full Name*' required /><br />
+                        <p>Email*</p><input type="text" className='gs-input' placeholder='Email Address*' required /><br />
+                        <p>Subject*</p><input type="text" className='gs-input' placeholder='Subject*' required /><br />
+                        <p>Message*</p><textarea name="text" id="text" cols="30" rows="10" placeholder='Message...' required></textarea>
                         <p>&nbsp;</p>
                         <button className='gs-button' type='submit'>Submit</button>
                     </form>
-                </div>
-                <div className='gs-Contactmap'>
-
-                </div>
+                </div>             
             </div>
         </div>
-      </div>
       <Footer/>
     </div>
   )
