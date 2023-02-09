dee2c0f606e7bc8199557a352822d4d98d409997
Gabbie99 <khoinom2018@gmail.com>
2023-02-10 00:24:39 +0100

    chore: working on contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
         </div>
             <div className="gs-Contactflex">
                 <div className='gs-Contactmap'>
-                    <iframe src="" frameborder="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.599831452894!2d7.400188914059594!3d9.100169290377648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e7571054e4a95%3A0x7564b4270a60fc82!2s37%20Obi%20Okosi%20St%2C%20Gwarinpa%20900108%2C%20Abuja%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1675984871955!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0" aria-hidden="false" tabindex="0"></iframe>
                 </div>
                 <div className='gs-Contactform'>
                     <form action="submit">
