c21b3e97927acab241d8b303913088e4fef0aff4
Gabbie99 <khoinom2018@gmail.com>
2023-02-10 09:54:47 +0100

    chore: adding location

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -17,12 +17,28 @@ function Contact() {
         <div className="gs-Contact-text">
             <h1 className='gs-Contact-texthead'>Contact Us</h1>
                 <p className='gs-Contact-text-p'>
-                Please feel free to call us to discuss your upcoming project! For inquiries about employment, subcontracting for us, or information about our current project, please use the form below so we can connect you to the best person to help you.
+                Please feel free to call us to discuss your upcoming project! For inquiries about employment, contracting with us, or information about our current project, please use the form below so we can connect with you and give you quality work and service.
                 </p>
         </div>
             <div className="gs-Contactflex">
                 <div className='gs-Contactmap'>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.599831452894!2d7.400188914059594!3d9.100169290377648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e7571054e4a95%3A0x7564b4270a60fc82!2s37%20Obi%20Okosi%20St%2C%20Gwarinpa%20900108%2C%20Abuja%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1675984871955!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0" aria-hidden="false" tabindex="0"></iframe>
+                {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.599831452894!2d7.400188914059594!3d9.100169290377648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e7571054e4a95%3A0x7564b4270a60fc82!2s37%20Obi%20Okosi%20St%2C%20Gwarinpa%20900108%2C%20Abuja%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1675984871955!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" ></iframe> */}
+                <div className="gs-Contactaddress">
+                  <p>
+                    Grandscape Residence's Limited, Company
+                  </p>
+                  <div className="gs-Contactadress-icon">
+                    <MapPin size={32} weight="thin" />
+                    <p>No 37 Obiokosi Street Gwarimpa, Abuja</p>
+                  </div>
+                  <div className="gs-Contactadress-icon">
+                    <WhatsappLogo size={32} weight="thin" />
+                    <a href="tel:+234 803-586-7102"></a>
+                  </div>
+                  <div className="gs-Contactadress-icon">
+
+                  </div>
+                </div>
                 </div>
                 <div className='gs-Contactform'>
                     <form action="submit">
