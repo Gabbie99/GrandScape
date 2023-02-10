d36f81133996c0501130664d52a9eadf0b7b381a
Gabbie99 <khoinom2018@gmail.com>
2023-02-10 19:42:54 +0100

    chore:services

---
+++ src/components/service/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Services.css'
+import NavBar from '../navBar/NavBar'
+
+function Services() {
+  return (
+    <div className='gs-Servicescontainer'>
+      <div className="gs-Serviceswrap">
+        <NavBar/>
+      </div>
+    </div>
+  )
+}
+
+export default Services
