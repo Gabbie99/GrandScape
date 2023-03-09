60003f6eeecb6620c85ac16eaff33e1ced3df162
Gabbie99 <khoinom2018@gmail.com>
2023-03-09 20:49:59 +0100

    chore; editing about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,7 +17,7 @@ function About() {
       <div className="gs-Aboutstory">
         <h1 className="gs-Aboutstory-head">Our Story</h1>
         <p className='gs-Aboutstory-para'>
-          Grandscape Residence's Ltd is a real estate firm stationed to provide real estate solutions to our clients. The company engages in consultation, real estate deveopment, management and interior desin services. <br /> In 10+ years of the existence of Grandscape, the company has helped individuals relaize their dreams of home ownership as well as office complexes. Our clientele comprises of private individuals, corporate bodies, corporate institutions and companies. With over a dcade of well grounded and sound experience, or focus is centralized on building unrivalled properties in Nigeria as a whole. 
+          Grandscape Residence's Ltd is a real estate firm stationed to provide real estate solutions to our clients. The company engages in consultation, real estate deveopment, management and interior desin services. <br /> In 10+ years of the existence of Grandscape, the company has helped individuals relaize their dreams of home ownership as well as office complexes. Our clientele comprises of private individuals, corporate bodies, corporate institutions and companies. 
         </p>
         <p>&nbsp;</p>
         </div>
@@ -27,7 +27,7 @@ function About() {
           </div>
           <div className="gs-Abouttell">
             <p className="gs-Aboutstory-para">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis tempore repellat incidunt et sed aspernatur, sit iusto ipsa beatae! Ipsam asperiores possimus, expedita vero nemo tenetur optio odio impedit. Architecto. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis facere natus ipsam at illo doloremque quibusdam quas! Delectus possimus explicabo voluptatum nostrum, maiores accusamus enim tenetur non ipsum nihil rem.
+            With over a decade of well grounded and sound experience, or focus is centralized on building unrivalled properties in Nigeria as a whole. <br />A company driven by passion and guided by ethics and morality, Granscape is commited to set and keep to high standards that have preceeded past projects. The company is presently obtaining high end projects to add to our grand portfolio.
             </p>
             <p>&nbsp;</p>
           </div>
