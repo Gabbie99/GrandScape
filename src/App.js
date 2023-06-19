a2ca8623e43eeddedd35c1c461e113ec1cf880e5
Gabbie99 <khoinom2018@gmail.com>
2023-06-19 17:58:02 +0100

    chore: edits and additives

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Project from './components/project/Project';
 function App() {
   return (
 
-    <div className="App">
+    <div className="App"> 
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<GeneralHomePage/>}/>
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import './NavBar.css'
+import Grandlogo from '../../assets/grandimage.png'
 import Hamburger from '.././hamBurger/Hamburger'
 import {Link} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div className='grand-NavContainer'>
-      <div className="grand-Logo">
-        <h1 className='grand-Navhead'>GrandScape</h1>
-      </div>
-      <div className="grand-Linklist">
-        <ul className='grand-ul'>
-          <Link to='/' className="grand-li"><div>Home</div></Link>
-          <Link to='/about' className="grand-li"><div>About</div></Link>
-          <Link to='/services' className="grand-li"><div>Services</div></Link>
-          <Link to='/projects' className="grand-li"><div>Projects</div></Link>
-          {/* <Link to='/testimonials' className="grand-li"><div>Testimonials</div></Link> */}
-          <Link to='/contact' className="grand-li"><div>Contact</div></Link>
-        </ul>
-      </div>
-      <div className="grand-hamburger">
-        <Hamburger/>
-      </div>
+    <div className='grand-Container'>
+      <div className="grand-NavContainer">
+        <div className="grand-Logo">
+          <img src={Grandlogo} alt="grand-logo" width={60} />
+          <h4>Grandsape</h4>
+        </div>
+          <div className="grand-Linklist">
+            <ul className='grand-ul'>
+              <Link to='/' className="grand-li"><div>Home</div></Link>
+              <Link to='/about' className="grand-li"><div>About</div></Link>
+              <Link to='/services' className="grand-li"><div>Services</div></Link>
+              <Link to='/projects' className="grand-li"><div>Projects</div></Link>
+              {/* <Link to='/testimonials' className="grand-li"><div>Testimonials</div></Link> */}
+              <Link to='/contact' className="grand-li"><div>Contact</div></Link>
+            </ul>
+          </div>
+          <div className="grand-hamburger">
+            <Hamburger/>
+          </div>
+      </div>     
     </div>
   )
 }
